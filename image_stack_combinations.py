721a7da0dfcd9e3ae027d7e910b688c091ac64a9
sharkgorilla176 <xharik176@gmail.com>
2022-05-30 12:18:38 +0200

    create directory of all files with layer numbering

--- image_stack_combinations.py
+++ image_stack_combinations.py
@@ -1,10 +1,22 @@
 'date created'
 'authors'
 
-import PIL as pillow
+import PIL.Image as pillow
 import os
 
 project_dir = r"C:\Users\sharikgorilla176\OneDrive\Pictures\NFTs\NAWT\Generate"
 
-layers_list = [item[1] for item in os.walk(project_dir)][0]
-
+#create list of directory for all files with layer numbering
+layers_list = []
+for root, dirs, files in os.walk(project_dir):
+    if len(files) != 0:
+        for file in files:
+            if file[0].isdigit() == False:
+                print(file, 'please label your filename as instructed in README.md')
+            else:
+                layers_list.append(root + "\\" + file)
+            
+            
+        
+for layers in layers_list:
+    print(layers.split("\\")[-1])
